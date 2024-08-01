dd1e6fc3d365b221122505374d166dd90bc9638a
Jayaanmol Shah <156838714+JayaanmolShah@users.noreply.github.com>
2024-08-01 17:58:40 +0530

    Add files via upload

--- signext.py
+++ signext.py
@@ -14,7 +14,7 @@ hide_menu="""
 """
 
 st.set_page_config(
-    page_icon="signext_logo1.png",
+    page_icon="images\signext_logo1.png",
     page_title="Signext"
 )
 
@@ -27,7 +27,7 @@ my_list = []
 def main():
     st.markdown(hide_menu,unsafe_allow_html=True)
     st.session_state['page'] = 'welcome_page'
-    st.sidebar.image('signext_logo.png')
+    st.sidebar.image('images\signext_logo.png')
     selected = option_menu(None, ["Home", 'Start Expressing','About Us',"How to Use"], icons=['house','balloon-heart','file-earmark-person','patch-question'], menu_icon="cast", default_index=1,orientation="horizontal")
     if selected=="Home":
         st.session_state['page'] = 'welcome_page'
@@ -74,7 +74,7 @@ def about_us_page():
     st.markdown("<h3 style='text-align: center;color:#b41c0c'>MEET THE TEAM</h3>", unsafe_allow_html=True)
     st.header('')
     t1,t2=st.columns(2)
-    t1.image('coat jaya.jpeg')
+    t1.image('images\coat jaya.jpeg')
     t2.markdown("<h4 style='color:#4cd4bc'>Jayaanmol Shah</h4>", unsafe_allow_html=True)
     t2.header("")
     t2.write("I'm currently pursuing my B.Tech in Computer Science at AISSMS College, Pune, following diploma in CS from Wadia College. As a passionate full-stack web developer, I enjoy bringing digital ideas to life. Outside of coding, you'll often find me immersed in the world of painting. Let's connect and explore the realms of technology and art together!")
@@ -86,7 +86,7 @@ def about_us_page():
     st.header('')
 
     u1,u2=st.columns(2)
-    u2.image('upeksha.jpeg')
+    u2.image('images/upeksha.jpeg')
     u1.markdown("<h4 style='color:#4cd4bc'>Upeksha Kohak</h4>", unsafe_allow_html=True)
     u1.header("")
     u1.write("I am a dedicated Computer Science student at AISSMS College of Engineering Pune, pursuing my BTech degree with a solid foundation in web development skills acquired through diploma  in Government Polytechnic pune. With hands-on experience in web development technologies, including HTML, CSS, JavaScript, and more.")
@@ -97,7 +97,7 @@ def about_us_page():
     st.header('')
 
     r1,r2=st.columns(2)
-    r1.image('raj.jpeg')
+    r1.image('images/raj.jpeg')
     r2.header("")
     r2.markdown("<h4 style='color:#4cd4bc'>Rajnandini Patil</h4>", unsafe_allow_html=True)
     r2.header("")
@@ -109,11 +109,11 @@ def about_us_page():
     st.header('')
 
     m1,m2=st.columns(2)
-    m2.image('raj.jpeg')
+    m2.image('images/iffa.jpeg')
     m1.header("")
-    m1.markdown("<h4 style='color:#4cd4bc'>Rajnandini Patil</h4>", unsafe_allow_html=True)
+    m1.markdown("<h4 style='color:#4cd4bc'>Shaikh Iffa</h4>", unsafe_allow_html=True)
     m1.header("")
-    m1.write("I am currently pursuing a Bachelor of Engineering degree at AISSMSCOE in Pune. Prior to this, I completed a diploma in Computer Engineering at ICRE Polytechnic. I am actively engaged in Android app development and concurrently studying web development.")
+    m1.write("I am Shaikh Iffa. Completed my diploma in Computer Engineering form JSPM'S polytechnic. Now studying B.E in Computer Engineering from AISSMS COE .Passionate about learning new technologies in technical field (Coding). Besides engineer I am a graphic designer (Digital Artist). Exploring about merging tech & creativity.")
     b1,b2,b3=m1.columns(3)
     b1.markdown('<a href="https://www.instagram.com/jazz__arts/" target="_blank"><img src="https://cdn-icons-png.flaticon.com/512/1384/1384031.png" alt="Instagram" width="30" height="30"></a>', unsafe_allow_html=True)
     b2.markdown('<a href="https://github.com/JayaanmolShah" target="_blank"><img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="Instagram" width="30" height="30"></a>', unsafe_allow_html=True)
@@ -128,8 +128,8 @@ def how_to_use_page():
 
     st.write("1. **Enable Webcam:** Click on the 'Use Webcam' button to start using the webcam for gesture recognition.")
     st.write("2. **Gesture Recognition:** Once the webcam is enabled, make hand gestures in front of the camera. Signext will recognize your gestures and translate them into text.")
-    st.image('alphabet.png')
-    st.image('number.jpg')
+    st.image('images/alphabet.png')
+    st.image('images/number.jpg')
     st.write("3. **Interpret Gestures:** Signext can recognize various gestures, such as numbers and letters in sign language. Perform these gestures clearly in front of the camera.")
     st.write("4. **View Translated Text:** As you perform gestures, Signext will display the corresponding text translation on the screen.")
     st.write("5. **Stop Gesture Recognition:** To stop gesture recognition, click the 'Stop Expressing' button.")
